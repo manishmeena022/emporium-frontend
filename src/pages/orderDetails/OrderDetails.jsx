2af05daeeda982d1e4419ce8ab4bc7cea34eefc0
Manish  meena <49023276+manishmeena022@users.noreply.github.com>
2023-08-23 22:47:00 +0530

    Update OrderDetails.jsx

--- src/pages/orderDetails/OrderDetails.jsx
+++ src/pages/orderDetails/OrderDetails.jsx
@@ -30,7 +30,7 @@ export const OrderDetails = () => {
               <b>Order Id : </b>{order.id}
             </p>
             <p>
-              <b>Order Amount : </b> ${order.orderAmount}
+              <b>Order Amount : </b> ₹{order.orderAmount}
             </p>
             <p>
               <b>Order Status : </b>{order.orderStatus}
